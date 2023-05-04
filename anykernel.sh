dba850ba77ea2fea8f24ec78fb2fc36760a2e830
Noah Brown <noahbrown94@pm.me>
2023-05-04 21:16:52 +0530

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=kernel by 秋秋 @Coolapk && LibXZR @Github
+kernel.string=kernel by iminamoto@Github
 do.devicecheck=0
 do.modules=0
 do.systemless=0
